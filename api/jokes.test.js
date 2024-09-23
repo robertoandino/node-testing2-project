395f90a2f6682b23d98e4484f9febc1e4b8e163e
Roberto Andino <andinorobert518@yahoo.com>
2024-09-23 12:03:22 -0400

    Done with tests

---
+++ api/jokes.test.js
@@ -0,0 +1,60 @@
+const request = require("supertest")
+const db = require("../dbConfig.js")
+const server = require("../server.js")
+const Joke = require("./jokesModel.js")
+
+const joke1 = {joke: "why did the chicken cross the road?", punchline:"Because it was free range"}
+const joke2 = {joke: "why did the chicken cross the road?", punchline:"To avoid this lame and outdate jokes"}
+
+beforeAll(async ()=>{
+    await db.migrate.rollback()
+    await db.migrate.latest()
+})
+
+beforeEach(async ()=>{
+    await db("jokes").truncate()
+})
+
+afterAll(async ()=>{
+    await db.destroy()
+})
+
+it("correct env var", ()=>{
+    expect(process.env.DB_ENV).toBe("testing")
+})
+
+describe("Jokes model functions", ()=>{
+    describe("create joke", ()=>{
+        it("add joke to the db", async ()=>{
+            let jokes
+            await Joke.createJoke(joke1)
+            jokes = await db("jokes")
+            expect(jokes).toHaveLength(1)
+
+            await Joke.createJoke(joke2)
+            jokes = await db("jokes")
+            expect(jokes).toHaveLength(2)
+        })
+        it("inserted joke and punchline", async ()=>{
+            const joke = await Joke.createJoke(joke1)
+            expect(joke).toMatchObject({joke_id:1,...joke})
+        })
+    })
+
+    describe("[DELETE] / deleted joke", ()=>{
+        it("removes joke from db", async ()=>{
+            const [joke_id] = await db("jokes").insert(joke1)
+            let joke = await db("jokes").where({joke_id}).first()
+            expect(joke).toBeTruthy()
+            await request(server).delete("/jokes/"+ joke_id)
+            joke = await db("jokes").where({joke_id}).first()
+            expect(joke).toBeFalsy()
+        })
+        it("respond with deleted joke", async ()=>{
+            await db("jokes").insert(joke1)
+            let joke = await request(server).delete("/jokes/1")
+            expect(joke.body).toMatchObject(joke1)
+        })
+    })
+
+})
--- knexfile.js
+++ knexfile.js
@@ -7,8 +7,28 @@ module.exports = {
 
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
       filename: './dev.sqlite3'
+    },
+    migrations:{
+      directorty: "./migrations"
+    },
+    seeds:{
+      directory: "./seeds"
+    }
+  },
+  testing: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './test.sqlite3'
+    },
+    migrations:{
+      directorty: "./migrations"
+    },
+    seeds:{
+      directory: "./seeds"
     }
   },
 
